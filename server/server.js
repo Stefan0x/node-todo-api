d40683cec3b0995705d66f2f738bcff37b295cfe
Stefan Euchenhofer <stefan.euchenhofer@gmail.com>
2017-09-27 14:24:49 +0200

    Added API

--- server/server.js
+++ server/server.js
@@ -1,11 +1,12 @@
 //Libriary Imports
 var express = require('express');
 var bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 // Local Imports
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
-var {User} = require('./models/todo');
+var {User} = require('./models/user');
 
 var app = express();
 
@@ -31,6 +32,29 @@ app.get('/todos', (req, res) => {
     });
   }, (e) => {
     res.status(400).send(e);
+  });
+});
+
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  // validate id using isValid
+  if (!ObjectID.isValid(id)) {
+    // if not -> 404 send back empty body
+    return res.status(404).send();
+  }
+
+  // find by id
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      // if no todo - send back 404 with empty body
+      return res.status(404).send();
+    }
+    // Happypath
+    // success if todo - send it back
+    res.status(200).send({todo});
+  }, (e) => {
+    res.status(400).send();
   });
 });
 
