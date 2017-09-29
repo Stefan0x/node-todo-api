e65647afbc20a34cccc0eb051e3644a701d7bc77
Stefan Euchenhofer <stefan.euchenhofer@gmail.com>
2017-09-29 16:26:35 +0200

    Added Test for PATCH

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -11,7 +11,9 @@ const todos = [{
   text: 'First test todo'
 }, {
   _id: new ObjectID(),
-  text: 'Second test todo'
+  text: 'Second test todo',
+  completed: true,
+  completedAt: 333
 }, {
   text: 'Third test todo'
 }];
@@ -141,6 +143,46 @@ describe('DELETE /todos/:id', () => {
     request(app)
       .delete('/todos/' + nonObjectId)
       .expect(404)
+      .end(done);
+  });
+});
+
+describe('PATCH /todos/:id', () => {
+  it('should update the todo', (done) => {
+    var hexId = todos[0]._id.toHexString();
+    var newTodo = {
+      text: 'Updated text',
+      completed: true
+    };
+
+    request(app)
+      .patch('/todos/' + hexId)
+      .send(newTodo)
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo.text).toBe(newTodo.text);
+        expect(res.body.todo.completed).toBe(true);
+        expect(typeof res.body.todo.completedAt).toBe('number');
+      })
+      .end(done);
+  });
+
+  it('should clear completedAt when todo is not completed', (done) => {
+    var hexId = todos[1]._id.toHexString();
+    var newTodo = {
+      text: 'Updated text',
+      completed: false
+    };
+
+    request(app)
+      .patch('/todos/' + hexId)
+      .send(newTodo)
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo.text).toBe(newTodo.text);
+        expect(res.body.todo.completed).toBe(false);
+        expect(res.body.todo.completedAt).toBe(null);
+      })
       .end(done);
   });
 });
